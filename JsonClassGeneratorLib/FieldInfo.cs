59a38a89719e715696c54280dd12878145178c41
Dai Rees <dai@dai.me>
2021-03-04 20:55:39 -0800

    Tidying up FieldInfo.cs.

--- JsonClassGeneratorLib/FieldInfo.cs
+++ JsonClassGeneratorLib/FieldInfo.cs
@@ -3,6 +3,7 @@
 using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 
@@ -10,101 +11,121 @@ namespace Xamasoft.JsonClassGenerator
 {
     public class FieldInfo
     {
-
-        public FieldInfo(IJsonClassGeneratorConfig generator, string jsonMemberName, JsonType type, bool usePascalCase, bool useJsonAttributes, bool useJsonPropertyName, IList<object> Examples)
+        public FieldInfo(
+            IJsonClassGeneratorConfig generator,
+            string                    jsonMemberName,
+            JsonType                  type,
+            bool                      usePascalCase,
+            bool                      useJsonAttributes,
+            bool                      useJsonPropertyName,
+            IReadOnlyList<object>     examples
+        )
         {
-            this.generator = generator;
-            this.JsonMemberName = jsonMemberName;
-            this.MemberName = jsonMemberName;
-            bool constainsSpecialChars = isContainsSpecialChars(MemberName);
-            ContainsSpecialChars = constainsSpecialChars;
-            if (usePascalCase || useJsonAttributes || useJsonPropertyName || constainsSpecialChars) MemberName = JsonClassGenerator.ToTitleCase(MemberName);
-            this.Type = type;
-            this.Examples = Examples;
-        }
-        private IJsonClassGeneratorConfig generator;
-        public string MemberName { get; private set; }
-        public string JsonMemberName { get; private set; }
-        public JsonType Type { get; private set; }
-        public IList<object> Examples { get; private set; }
-        public bool ContainsSpecialChars { get; set; }
-        public int MyProperty { get; set; }
+            this.generator             = generator       ?? throw new ArgumentNullException(nameof(generator));
+            this.JsonMemberName        = jsonMemberName  ?? throw new ArgumentNullException(nameof(jsonMemberName));
+            this.MemberName            = jsonMemberName;
+            this.ContainsSpecialChars  = IsContainsSpecialChars(this.MemberName);
 
-        private bool isContainsSpecialChars(string str)
-        {
-            for (int i = 0; i < str.Length; i++)
+            if (usePascalCase || useJsonAttributes || useJsonPropertyName || this.ContainsSpecialChars)
             {
-                var c = str[i];
-                if (!char.IsLetterOrDigit(c) && c!= '_')
+                this.MemberName = JsonClassGenerator.ToTitleCase(this.MemberName);
+            }
+            
+            this.Type     = type ?? throw new ArgumentNullException(nameof(type));
+            this.Examples = examples ?? Array.Empty<object>();
+        }
+
+        private readonly IJsonClassGeneratorConfig generator;
+
+        public string                MemberName     { get; }
+        public string                JsonMemberName { get; }
+        public JsonType              Type           { get; }
+        public IReadOnlyList<object> Examples       { get; }
+
+        public bool ContainsSpecialChars { get; set; }
+        public int  MyProperty           { get; set; }
+
+        private static bool IsContainsSpecialChars(string text)
+        {
+            for (int i = 0; i < text.Length; i++)
+            {
+                char c = text[i];
+                if (!char.IsLetterOrDigit(c) && c != '_')
                 {
                     return true;
                 }
             }
+
             return false;
         }
 
-        internal static string ToTitleCase(string str)
+        internal static string ToTitleCase(string text)
         {
-            var sb = new StringBuilder(str.Length);
-            var flag = true;
+            if (text is null) throw new ArgumentNullException(nameof(text));
+            if (String.IsNullOrWhiteSpace(text)) return text;
 
-            for (int i = 0; i < str.Length; i++)
+            var sb = new StringBuilder(text.Length);
+            var lastCharWasSpecial = true;
+
+            for (int i = 0; i < text.Length; i++)
             {
-                var c = str[i];
+                var c = text[i];
                 if (char.IsLetterOrDigit(c))
                 {
-                    sb.Append(flag ? char.ToUpper(c) : c);
-                    flag = false;
+                    sb.Append(lastCharWasSpecial ? char.ToUpper(c) : c);
+                    lastCharWasSpecial = false;
                 }
                 else
                 {
-                    flag = true;
+                    lastCharWasSpecial = true;
                 }
             }
+
             return sb.ToString();
         }
 
-        public string GetGenerationCode(string jobject)
+        public string GetGenerationCode(string jObject)
         {
-            var field = this;
-            if (field.Type.Type == JsonTypeEnum.Array)
+            if (jObject is null) throw new ArgumentNullException(nameof(jObject));
+
+            if (this.Type.Type == JsonTypeEnum.Array)
             {
-                var innermost = field.Type.GetInnermostType();
-                return string.Format("({1})JsonClassHelper.ReadArray<{5}>(JsonClassHelper.GetJToken<JArray>({0}, \"{2}\"), JsonClassHelper.{3}, typeof({6}))",
-                    jobject,
-                    field.Type.GetTypeName(),
-                    field.JsonMemberName,
+                JsonType innermost = this.Type.GetInnermostType();
+                return string.Format(CultureInfo.InvariantCulture, "({1})JsonClassHelper.ReadArray<{5}>(JsonClassHelper.GetJToken<JArray>({0}, \"{2}\"), JsonClassHelper.{3}, typeof({6}))",
+                    jObject,
+                    this.Type.GetTypeName(),
+                    this.JsonMemberName,
                     innermost.GetReaderName(),
                     -1,
                     innermost.GetTypeName(),
-                    field.Type.GetTypeName()
-                    );
+                    this.Type.GetTypeName()
+                );
             }
-            else if (field.Type.Type == JsonTypeEnum.Dictionary)
+            else if (this.Type.Type == JsonTypeEnum.Dictionary)
             {
-
-                return string.Format("({1})JsonClassHelper.ReadDictionary<{2}>(JsonClassHelper.GetJToken<JObject>({0}, \"{3}\"))",
-                    jobject,
-                    field.Type.GetTypeName(),
-                    field.Type.InternalType.GetTypeName(),
-                    field.JsonMemberName,
-                    field.Type.GetTypeName()
-                    );
+                return string.Format(CultureInfo.InvariantCulture, "({1})JsonClassHelper.ReadDictionary<{2}>(JsonClassHelper.GetJToken<JObject>({0}, \"{3}\"))",
+                    jObject,
+                    this.Type.GetTypeName(),
+                    this.Type.InternalType.GetTypeName(),
+                    this.JsonMemberName,
+                    this.Type.GetTypeName()
+                );
             }
             else
             {
-                return string.Format("JsonClassHelper.{1}(JsonClassHelper.GetJToken<{2}>({0}, \"{3}\"))",
-                    jobject,
-                    field.Type.GetReaderName(),
-                    field.Type.GetJTokenType(),
-                    field.JsonMemberName);
+                return string.Format(CultureInfo.InvariantCulture, "JsonClassHelper.{1}(JsonClassHelper.GetJToken<{2}>({0}, \"{3}\"))",
+                    jObject,
+                    this.Type.GetReaderName(),
+                    this.Type.GetJTokenType(),
+                    this.JsonMemberName
+                );
             }
 
         }
 
         public string GetExamplesText()
         {
-            return string.Join(", ", Examples.Take(5).Select(x => JsonConvert.SerializeObject(x)).ToArray());
+            return string.Join(separator: ", ", values: this.Examples.Take(5).Select(x => JsonConvert.SerializeObject(x)));
         }
 
     }
