8bdd52aee3db4c98ffe3ce863c3770a5ee60ab04
HoLLy <dario.db@hotmail.com>
2021-08-23 21:57:20 +0200

    Add additional tests for Newtonsoft.Json, with and without attributes

--- TESTS-JSON-to-CSHARP/Test_17_RECORD_TYPES.cs
+++ TESTS-JSON-to-CSHARP/Test_17_RECORD_TYPES.cs
@@ -12,8 +12,25 @@ namespace TESTS_JSON_TO_CSHARP
         [TestMethod]
         public void Run()
         {
+            DoTest(null);
+        }
+
+        [TestMethod]
+        public void RunNewtonsoft()
+        {
+            DoTest(false, "_NEWTONSOFT");
+        }
+
+        [TestMethod]
+        public void RunSystemTextJson()
+        {
+            DoTest(true, "_SYSTEMTEXTJSON");
+        }
+
+        private static void DoTest(bool? useSystemTextJson, string outputFileSuffix = "")
+        {
             string inputPath = Directory.GetCurrentDirectory().Replace("bin\\Debug", "") + @"Test_17_RECORD_TYPES_INPUT.txt";
-            string outputPath = Directory.GetCurrentDirectory().Replace("bin\\Debug", "") + @"Test_17_RECORD_TYPES_OUTPUT.txt";
+            string outputPath = Directory.GetCurrentDirectory().Replace("bin\\Debug", "") + $@"Test_17_RECORD_TYPES_OUTPUT{outputFileSuffix}.txt";
             string input = File.ReadAllText(inputPath);
             string output = File.ReadAllText(outputPath);
 
@@ -21,7 +38,8 @@ namespace TESTS_JSON_TO_CSHARP
             {
                 CodeWriter = new CSharpCodeWriter(),
                 RecordTypes = true,
-                UseJsonPropertyName = true, // TODO: also test with newtonsoft json
+                UseJsonPropertyName = useSystemTextJson == true,
+                UseJsonAttributes = useSystemTextJson == false,
             };
 
             string actual = jsonClassGenerator.GenerateClasses(input, out _).ToString();
